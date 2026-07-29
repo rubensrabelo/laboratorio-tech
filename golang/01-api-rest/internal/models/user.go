3fbb1b213fddaa33f28c4b2674d633f180f87ada
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-07-29 09:18:20 -0300

    source(01-api-rest): Creating a REST API in Go (Clean Architecture + Design Patterns) – Updated Version (wip)

--- golang/01-api-rest/internal/models/user.go
+++ golang/01-api-rest/internal/models/user.go
@@ -1,6 +1,14 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrFieldsRequired = errors.New("nome e email sao obrigatorios")
+	ErrEmailExists    = errors.New("email ja cadastrado")
+)
 
 type User struct {
 	ID    uuid.UUID `json:"id"`
--- golang/01-api-rest/internal/repositories/memory_user_repository.go
+++ golang/01-api-rest/internal/repositories/memory_user_repository.go
@@ -1,20 +1,25 @@
 package repositories
 
 import (
-	"errors"
 	"rest-api-2/internal/models"
 	"sync"
 )
 
 type MemoryUserRepository struct {
-	mu    sync.Mutex
-	users []models.User
+	mu     sync.Mutex
+	users  []models.User
+	emails map[string]bool
 }
 
 func NewMemoryUserRepository() *MemoryUserRepository {
 	return &MemoryUserRepository{
-		users: []models.User{},
+		users:  []models.User{},
+		emails: make(map[string]bool),
 	}
+}
+
+func (r *MemoryUserRepository) emailExists(email string) bool {
+	return r.emails[email]
 }
 
 func (r *MemoryUserRepository) Add(user models.User) error {
@@ -22,9 +27,14 @@ func (r *MemoryUserRepository) Add(user models.User) error {
 	defer r.mu.Unlock()
 
 	if user.Name == "" || user.Email == "" {
-		return errors.New("nome e email sao obrigatorios")
+		return models.ErrFieldsRequired
 	}
 
+	if r.emailExists(user.Email) {
+		return models.ErrEmailExists
+	}
+
+	r.emails[user.Email] = true
 	r.users = append(r.users, user)
 	return nil
 }
